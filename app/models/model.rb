7b5b04479abad9cf322d0e258fdc00dfe84d3b17
angelinasongco <angelinasongco123@gmail.com>
2018-07-18 17:39:44 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,10 @@
 class Model
-  # Replace with Dog clas
+  # THIS IS WHERE THE RUBY HAPPENS (BACKEND)
 end
+
+def get_fortune
+  fortunes_array= ["You suck","Melody will bully you","Erina will love you","Your AP Scores will be straight 5s","Erina will go to USC","You will get ran over a truck","You'll fall soon"]
+  return fortunes_array.sample 
+end
+
+puts get_fortune
